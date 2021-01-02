200469edadf8095a31fab34b678384cddc5fb414
ohnoshobu <ohno56sent@gmail.com>
2021-01-02 13:55:41 +0900

    jQery学習

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-document.write('せせ');
-window.resizeTo(600, 300);
+$(function(){
+	$('.box1').mouseover(function(){
+		$('.box1').css({'background-color': '#0000FF'})
+	});
+	$('.box1').mouseout(function(){
+		$('.box1').css({'background-color': '#FF0000'})
+	});
+});
